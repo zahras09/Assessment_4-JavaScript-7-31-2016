70db0194a4c6f352a28b635b4bda6a1f4b2b8084
Zahra Sadat <zahras09@gmail.com>
2016-07-31 19:17:49 -0700

    sql exercises

---
+++ answers.sql
@@ -0,0 +1,70 @@
+1
+
+-----
+
+Select all fields for all brands in the brands table.
+
+The result set for this query should be every record in the brands table.
+
+-----
+
+
+select * from brands
+;
+
+
+==========
+2
+
+-----
+
+Select all fields for all car models made by Pontiac in the 
+models table. 
+
+The result set should be:
+  id   | year  | brand_name |    name
+-------+-------+------------+------------
+    25 |  1961 | Pontiac    | Tempest
+    27 |  1962 | Pontiac    | Grand Prix
+    36 |  1963 | Pontiac    | Grand Prix
+    42 |  1964 | Pontiac    | GTO
+    43 |  1964 | Pontiac    | LeMans
+    44 |  1964 | Pontiac    | Bonneville
+    45 |  1964 | Pontiac    | Grand Prix
+(7 rows)
+
+
+-----
+
+
+select * from models where brand_name = 'Pontiac';
+
+
+==========
+3
+
+-----
+
+Select the brand name and model name for all models made in 
+1964 from the models table. 
+
+The result set should be:
+ brand_name |    name
+------------+-------------
+ Chevrolet  | Corvette
+ Ford       | Mustang
+ Ford       | Galaxie
+ Pontiac    | GTO
+ Pontiac    | LeMans
+ Pontiac    | Bonneville
+ Pontiac    | Grand Prix
+ Plymouth   | Fury
+ Studebaker | Avanti
+ Austin     | Mini Cooper
+ (10 rows)
+ 
+
+-----
+
+
+select brand_name, name from models where year = 1964;
